3ccb095f3e0a3666a390c732eb1affff1d01c45c
Austin Smith <datahub@Mesospheric-Spoonbill.local>
2025-03-08 14:35:45 -0500

    New Header

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/design-builder/',
   plugins: [react()],
+  logLevel: 'info',
+  server: {
+    open: true, // Opens browser automatically
+  }
 })
